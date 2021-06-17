8c627d4f3b5a6a9265d32b153d2bd00350aec067
VeryLoveLoli <chuancan@icloud.com>
2021-06-17 10:09:22 +0800

    支持 8K/16K/32K、16bit、单双通道

--- Sources/WebRTCNSSwift/WebRTCNSSwift.swift
+++ Sources/WebRTCNSSwift/WebRTCNSSwift.swift
@@ -37,6 +37,9 @@ open class WebRTCNSSwift {
      1. 仅 8K、16K、32K、48K（不同版本支持的采样率不一样）
      2. 采样率、采样位数、通道数，和原音频一致才能更好的降噪，不然会出现噪音
      3. 无噪音音频，降噪会出现噪音
+     4. WebRTC 经测试 支持 8K、16K 16bit 单双通道 降噪
+     5. 支持 32K 16bit 单双通道
+     6. 自动转换PCM默认读取音频原信息，读取不到转 16K 16bit 单通道
      
      - parameter    inPath:                 输入音频路径
      - parameter    outPath:                输出音频路径
@@ -60,6 +63,9 @@ open class WebRTCNSSwift {
      1. 仅 8K、16K、32K、48K（不同版本支持的采样率不一样）
      2. 采样率、采样位数、通道数，和原音频一致才能更好的降噪，不然会出现噪音
      3. 无噪音音频，降噪会出现噪音
+     4. WebRTC 经测试 支持 8K、16K 16bit 单双通道 降噪
+     5. 支持 32K 16bit 单双通道
+     6. 自动转换PCM默认读取音频原信息，读取不到转 16K 16bit 单通道
      
      - parameter    inPath:                 输入音频路径
      - parameter    outPath:                输出音频路径
@@ -96,32 +102,15 @@ open class WebRTCNSSwift {
             case 8000, 16000, 32000, 48000:
                 break
             default:
-                if sampleRate == 0 {
-                    sampleRate = 32000
-                }
-                else if sampleRate < 8000 {
-                    sampleRate = 8000
-                }
-                else if sampleRate < 16000 {
-                    sampleRate = 16000
-                }
-                else if sampleRate < 32000 {
-                    sampleRate = 32000
-                }
-                else if sampleRate < 48000 {
-                    sampleRate = 48000
-                }
-                else {
-                    sampleRate = 48000
-                }
+                sampleRate = 16000
             }
             
             if channels == 0 {
-                channels = 2
+                channels = 1
             }
             
             if bits == 0 {
-                bits = 32
+                bits = 16
             }
             
             var client = AudioStreamBasicDescription()
@@ -244,6 +233,8 @@ open class WebRTCNSSwift {
      1. 仅 8K、16K、32K、48K（不同版本支持的采样率不一样）
      2. 采样率、采样位数、通道数，和原音频一致才能更好的降噪，不然会出现噪音
      3. 无噪音音频，降噪会出现噪音
+     4. WebRTC 经测试 支持 8K、16K 16bit 单双通道 降噪
+     5. 支持 32K 16bit 单双通道
      
      - parameter    sampleRate:             采样率
      - parameter    bitsPer:                采样位数
@@ -322,6 +313,152 @@ open class WebRTCNSSwift {
      - parameter    bytes:  音频数据
      */
     open func handle(_ bytes: [UInt8]) -> [UInt8] {
+        
+        if channels == 1 {
+            
+            if sampleRate == 32000 {
+                
+                return handleDualChannel16K(bytes)
+            }
+            else {
+                
+                return handleSingleChannel16k(bytes)
+            }
+        }
+        else if channels == 2 {
+            
+            if sampleRate == 32000 {
+                
+                var s0 = [UInt8](repeating: 0, count: bytes.count/2)
+                var s1 = [UInt8](repeating: 0, count: bytes.count/2)
+                
+                for i in 0..<bytes.count/8 {
+                    
+                    s0[i*4] = bytes[i*8]
+                    s0[i*4+1] = bytes[i*8+1]
+                    s0[i*4+2] = bytes[i*8+4]
+                    s0[i*4+3] = bytes[i*8+5]
+                    
+                    s1[i*4] = bytes[i*8+2]
+                    s1[i*4+1] = bytes[i*8+3]
+                    s1[i*4+2] = bytes[i*8+6]
+                    s1[i*4+3] = bytes[i*8+7]
+                }
+                
+                let bytes0 = handleDualChannel16K(s0)
+                let bytes1 = handleDualChannel16K(s1)
+                
+                var outByte = [UInt8](repeating: 0, count: bytes.count)
+                
+                for i in 0..<bytes.count/8 {
+                    
+                    outByte[i*8] = bytes0[i*4]
+                    outByte[i*8+1] = bytes0[i*4+1]
+                    outByte[i*8+4] = bytes0[i*4+2]
+                    outByte[i*8+5] = bytes0[i*4+3]
+                    
+                    outByte[i*8+2] = bytes1[i*4]
+                    outByte[i*8+3] = bytes1[i*4+1]
+                    outByte[i*8+6] = bytes1[i*4+2]
+                    outByte[i*8+7] = bytes1[i*4+3]
+                }
+                
+                return outByte
+            }
+            else {
+                
+                return handleDualChannel16K(bytes)
+            }
+        }
+        else {
+            
+            return bytes
+        }
+    }
+    
+    /**
+     处理双通道16K采样率
+     
+     音频帧数必须10毫秒PCM音频数据
+     字节数 ` bytesCount = sampleRate / 100 * channels * bitsPer / 8`
+     
+     - parameter    bytes:  音频数据
+     */
+    open func handleDualChannel16K(_ bytes: [UInt8]) -> [UInt8] {
+        
+        let c = 2
+        
+        var outBytes = [UInt8](repeating: 0, count: bytes.count)
+        
+        /// 输入左通道
+        var in_c0 = [UInt8](repeating: 0, count: bytes.count/c)
+        /// 输入右通道
+        var in_c1 = [UInt8](repeating: 0, count: bytes.count/c)
+        /// 输出左通道
+        var out_c0 = [UInt8](repeating: 0, count: bytes.count/c)
+        /// 输出右通道
+        var out_c1 = [UInt8](repeating: 0, count: bytes.count/c)
+        
+        /// 拆分交错通道
+        for i in 0..<bytes.count/4 {
+            
+            in_c0[i*2] = bytes[i*4]
+            in_c0[i*2+1] = bytes[i*4+1]
+            
+            in_c1[i*2] = bytes[i*4+2]
+            in_c1[i*2+1] = bytes[i*4+3]
+        }
+        
+        /// 转换输入指针
+        in_c0.withUnsafeBytes { (inBody_0: UnsafeRawBufferPointer) -> Void in
+            
+            let inBind_0 = inBody_0.bindMemory(to: Int16.self)
+            let inBuffer_0 = inBind_0.baseAddress
+            
+            in_c1.withUnsafeBytes { (inBody_1: UnsafeRawBufferPointer) -> Void in
+                
+                let inBind_1 = inBody_1.bindMemory(to: Int16.self)
+                let inBuffer_1 = inBind_1.baseAddress
+                
+                /// 转换输出指针
+                out_c0.withUnsafeMutableBytes { (outBody_0: UnsafeMutableRawBufferPointer) -> Void in
+                    
+                    let outBind_0 = outBody_0.bindMemory(to: Int16.self)
+                    let outBuffer_0 = outBind_0.baseAddress
+                    
+                    out_c1.withUnsafeMutableBytes { (outBody_1: UnsafeMutableRawBufferPointer) -> Void in
+                        
+                        let outBind_1 = outBody_1.bindMemory(to: Int16.self)
+                        let outBuffer_1 = outBind_1.baseAddress
+                        
+                        /// 降噪
+                        WebRtcNsx_Process(nsx, [inBuffer_0, inBuffer_1], Int32(c), [outBuffer_0, outBuffer_1])
+                    }
+                }
+            }
+        }
+        
+        /// 交错通道
+        for i in 0..<bytes.count/4 {
+            
+            outBytes[i*4] = out_c0[i*2]
+            outBytes[i*4+1] = out_c0[i*2+1]
+            outBytes[i*4+2] = out_c1[i*2]
+            outBytes[i*4+3] = out_c1[i*2+1]
+        }
+        
+        return outBytes
+    }
+    
+    /**
+     处理单通道16K采样率
+     
+     音频帧数必须10毫秒PCM音频数据
+     字节数 ` bytesCount = sampleRate / 100 * channels * bitsPer / 8`
+     
+     - parameter    bytes:  音频数据
+     */
+    open func handleSingleChannel16k(_ bytes: [UInt8]) -> [UInt8] {
         
         var outBytes = [UInt8](repeating: 0, count: bytes.count)
         memcpy(&outBytes, bytes, bytes.count)
